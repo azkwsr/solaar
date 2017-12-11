6ec87ae513236372c200f6e3921cf2d2bdf97386
azukiwasher <azukiwasher@yahoo.co.jp>
2017-12-11 21:12:30 +0900

    Fix spec failures

--- spec/solaar/term_spec.rb
+++ spec/solaar/term_spec.rb
@@ -2,20 +2,21 @@
 require 'spec_helper'
 
 describe Solaar::Term do
+  let(:year) { Time.now().year }
   let(:solaar) { Solaar::Term.new }
-  let(:record) {{ date: "2014-12-07", language: { ja: "大雪", en: "Major snow" }}}
+  let(:record) {{ date: "#{year}-12-07", language: { ja: "大雪", en: "Major snow" }}}
 
   describe "#calc" do
     it "returns a collection of terms for this year" do
-      expect(solaar.calc.first[:date]).to match /2014/
+      expect(solaar.calc.first[:date]).to match /#{year}/
     end
 
     it "returns a collection of terms for the given year" do
-      expect(solaar.calc(year: 2014).size).to eql 24
+      expect(solaar.calc(year: year).size).to eql 24
     end
 
     it "returns a record for the given year and month" do
-      expect(solaar.calc(year: 2014, month: 12).first).to eql record
+      expect(solaar.calc(year: year, month: 12).first).to eql record
     end
 
     it "returns a record for the given month" do
